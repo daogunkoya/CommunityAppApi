7019c2e5b44cdab49c1848a1d6eeb0b7215d03e6
daniel <daniel@daniels-MBP-3.mynet>
2025-08-08 14:30:29 +0100

    Disable CSRF protection globally by commenting out VerifyCsrfToken middleware

--- api/index.php
+++ api/index.php
@@ -25,13 +25,58 @@ try {
         $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
     }
 
-    // Disable CSRF protection for API routes using environment variables
+    // Disable CSRF protection globally for API routes
     $_ENV['SESSION_DRIVER'] = 'array';
     $_ENV['SESSION_LIFETIME'] = '0';
     $_ENV['APP_ENV'] = 'production';
 
     // Bootstrap Laravel
     $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+    // Disable CSRF protection globally
+    $app->singleton('Illuminate\Contracts\Http\Kernel', function ($app) {
+        return new class($app) extends \Illuminate\Foundation\Http\Kernel {
+            protected $middleware = [
+                \App\Http\Middleware\TrustProxies::class,
+                \Illuminate\Http\Middleware\HandleCors::class,
+                \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+                \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+                \App\Http\Middleware\TrimStrings::class,
+                \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+            ];
+
+            protected $middlewareGroups = [
+                'web' => [
+                    \App\Http\Middleware\EncryptCookies::class,
+                    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                    \Illuminate\Session\Middleware\StartSession::class,
+                    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                    // \App\Http\Middleware\VerifyCsrfToken::class, // DISABLED CSRF
+                    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                ],
+
+                'api' => [
+                    \App\Http\Middleware\DisableCsrfForApi::class,
+                    \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+                    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                ],
+            ];
+
+            protected $middlewareAliases = [
+                'auth' => \App\Http\Middleware\Authenticate::class,
+                'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+                'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+                'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+                'can' => \Illuminate\Auth\Middleware\Authorize::class,
+                'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+                'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+                'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+                'signed' => \App\Http\Middleware\ValidateSignature::class,
+                'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+                'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+            ];
+        };
+    });
 
     // Run the application
     $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
